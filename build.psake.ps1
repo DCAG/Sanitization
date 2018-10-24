b2172633d19ce1ccd6eb8dfb5051e662a805fdfb
Amir Granot <amirgranotag@gmail.com>
2018-10-25 00:43:33 +0300

    move module file and manifest to source directory

--- build.psake.ps1
+++ build.psake.ps1
@@ -11,28 +11,40 @@ TaskTearDown {
     "Executing task tear down"
 }
 
-Task default -depends psscriptanalyzer
+Task default -depends test
 #Task default -depends build
 
-Task clean {
-    Remove-Item "$WorkingDir\bin"
+Task publish {
+
 }
 
-Task test -depends build {
-    $ModuleManifest = Test-ModuleManifest -Path "$ModuleName.psd1"
-    $ModuleManifest | fl * -Force
-    mkdir "$WorkingDir\bin\$ModuleName\$ModuleManifest" -Force
+Task test -depends deploy {
+    Invoke-Pester "$WorkingDir\Tests"
 }
 
-Task build -depends psscriptanalyzer {
+Task deploy -depends build {
+    Import-Module "$WorkingDir\bin\$ModuleName"
 }
 
-
-
-Task deploy -depends test {
-
+Task build -depends psscriptanalyzer, clean {
+    $SourceFolder = "$WorkingDir\Source\"
+    $ManifestFile = "$SourceFolder\$ModuleName.psd1"
+    
+    $ModuleManifest = Test-ModuleManifest -Path $ManifestFile
+    $ModuleVersion = $ModuleManifest.Version
+    $OutputFolder = "$WorkingDir\bin\$ModuleName\$ModuleVersion"
+    mkdir $OutputFolder -Force
+    Copy-Item -Path "$SourceFolder\*" -Destination "$OutputFolder\" -Recurse
 }
 
 Task psscriptanalyzer {
     Invoke-ScriptAnalyzer -Path "$WorkingDir\Source" -Recurse
+}
+
+Task clean {
+    Remove-Module ModuleName -ErrorAction SilentlyContinue
+    $binFolder = "$WorkingDir\bin"
+    if(Test-Path $binFolder -ErrorAction SilentlyContinue){
+        Remove-Item "$WorkingDir\bin" -Recurse -Force
+    }
 }
