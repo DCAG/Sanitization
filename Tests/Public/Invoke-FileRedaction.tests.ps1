c4c9f9e1384d7032dbdce706a7690b3780eea348
Amir Granot <amirgranotag@gmail.com>
2018-11-04 01:12:22 +0200

    fix tests

--- Tests/Public/Invoke-FileRedaction.tests.ps1
+++ Tests/Public/Invoke-FileRedaction.tests.ps1
@@ -1,10 +1,10 @@
 Describe 'Invoke-FileRedaction' {
     Context 'Parameters Validation' {
-        It 'Parameter RedactionRule should not be null' -Skip:([environment]::OSVersion.Platform -match 'Unix') {
+        It 'Parameter RedactionRule should not be null [Unix]' -Skip:(-not ([environment]::OSVersion.Platform -match 'Unix')) {
             Invoke-Expression 'ip addr' | Out-File "TestDrive:\IPConfig.txt"
             {Invoke-FileRedaction -Path "TestDrive:\IPConfig.txt" -RedactionRule $null} | Should -Throw -ExpectedMessage "Cannot bind argument to parameter 'RedactionRule' because it is null"
         }
-        It 'Parameter RedactionRule should not be null' -Skip:([environment]::OSVersion.Platform -match 'Win') {
+        It 'Parameter RedactionRule should not be null [Windows]' -Skip:(-not ([environment]::OSVersion.Platform -match 'Win')) {
             Invoke-Expression 'IPConfig /All' | Out-File "TestDrive:\IPConfig.txt"
             {Invoke-FileRedaction -Path "TestDrive:\IPConfig.txt" -RedactionRule $null} | Should -Throw -ExpectedMessage "Cannot bind argument to parameter 'RedactionRule' because it is null"
         }
--- build.psake.ps1
+++ build.psake.ps1
@@ -4,7 +4,7 @@ Function InstallRequiredModules {
     $InstalledModule = @(Get-InstalledModule -Name $RequiredModules -ErrorAction 'SilentlyContinue' | Select-Object -ExpandProperty Name)
     $ModuleToInstall = Compare-Object -ReferenceObject $RequiredModules -DifferenceObject $InstalledModule | Select-Object -ExpandProperty 'InputObject'
     if($ModuleToInstall.Count -gt 0){
-        Install-Module -Name $ModuleToInstall -Repository 'PSGallery' -Scope 'CurrentUser' -Force -ErrorAction 'Stop'
+        Install-Module -Name $ModuleToInstall -Scope 'CurrentUser' -Force -ErrorAction 'Stop'
     }
 
     Import-Module -Name $RequiredModules -Force -ErrorAction 'Stop'
