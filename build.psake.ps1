9c3344c536454c9203460f4957491697b4c13b33
Amir Granot <amirgranotag@gmail.com>
2018-11-04 01:05:09 +0200

    added seperate test for unix and seperate test for windows

--- build.psake.ps1
+++ build.psake.ps1
@@ -4,8 +4,7 @@ Function InstallRequiredModules {
     $InstalledModule = @(Get-InstalledModule -Name $RequiredModules -ErrorAction 'SilentlyContinue' | Select-Object -ExpandProperty Name)
     $ModuleToInstall = Compare-Object -ReferenceObject $RequiredModules -DifferenceObject $InstalledModule | Select-Object -ExpandProperty 'InputObject'
     if($ModuleToInstall.Count -gt 0){
-        Set-PSRepository -InstallationPolicy Trusted -Name PSGallery
-        Install-Module -Name $ModuleToInstall -Repository 'PSGallery' -Force -SkipPublisherCheck -Scope 'CurrentUser' -AllowClobber -Confirm:$false -ErrorAction 'Stop'
+        Install-Module -Name $ModuleToInstall -Repository 'PSGallery' -Scope 'CurrentUser' -Force -ErrorAction 'Stop'
     }
 
     Import-Module -Name $RequiredModules -Force -ErrorAction 'Stop'
