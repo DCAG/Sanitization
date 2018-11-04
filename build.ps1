0f3b4691928a2ad49691394472813c4832328521
Amir Granot <amirgranotag@gmail.com>
2018-11-04 22:58:33 +0200

    fail build and psake fail

--- build.ps1
+++ build.ps1
@@ -2,8 +2,8 @@
 
 # Builds the module by invoking psake on the build.psake.ps1 script.
 $BuildFile = Join-Path -Path $PSScriptRoot -ChildPath 'build.psake.ps1'
-Invoke-PSake -buildFile $BuildFile 
-#Invoke-PSake -buildFile $BuildFile -taskList Clean #Build
-#Invoke-PSake -buildFile $BuildFile -taskList CreateMarkdownHelp #Build
-#Invoke-PSake -buildFile $BuildFile -taskList UpdateMarkdownHelp #Build
-#Invoke-PSake -buildFile $BuildFile -taskList CreateExternalHelp #Build
+Invoke-PSake -buildFile $BuildFile -ErrorAction Stop
+#Invoke-PSake -buildFile $BuildFile -taskList Clean
+#Invoke-PSake -buildFile $BuildFile -taskList CreateMarkdownHelp
+#Invoke-PSake -buildFile $BuildFile -taskList UpdateMarkdownHelp
+#Invoke-PSake -buildFile $BuildFile -taskList CreateExternalHelp -ErrorAction Stop
